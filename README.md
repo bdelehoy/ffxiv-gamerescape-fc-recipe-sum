8a2756a813f7db5a847e7662d93c98fc583cb489
Brandon Delehoy <bdelehoy@yahoo.com>
2022-05-15 00:34:42 -0700

    Use argparse instead of sys.argv Also lay groundwork for CSV output

--- README.md
+++ README.md
@@ -2,18 +2,23 @@
 Parses a Gamer Escape wiki page for a craftable item in Final Fantasy XIV to generate a summary of all the ingredients needed for crafting that item. Intended for use with Free Company crafts, where recipes may have redundant ingredients spread across multiple phases (although this works with regular craftable items, too).
 
 # Requirements
-Any Python version that supports BeautifulSoup 4.10.0 (developed and working with 3.10.4)
+Any Python version that supports BeautifulSoup 4.10.0 (developed and working with Python 3.10.4)
 
 # Usage
-Run in a command-line environment. Only needs 1 argument: a URL to a Gamer Escape wiki page. Can also be a dedicated "Recipe" page.
+Run in a command-line environment. Brackets indicate optional arguments.
 
 ```
-python gamerescape-recipe-sum.py https://ffxiv.gamerescape.com/wiki/Medium_Cafe_Walls
-python gamerescape-recipe-sum.py https://ffxiv.gamerescape.com/wiki/Medium_Cafe_Walls/Recipe
-python gamerescape-recipe-sum.py https://ffxiv.gamerescape.com/wiki/Mythrite_Ingot
+gamerescape-recipe-sum.py [-h] -u URL [-o OUTPUT_FILE]
 ```
 
-Use of a virtual environment is highly recommended.
+Examples:
+```
+python gamerescape-recipe-sum.py -u https://ffxiv.gamerescape.com/wiki/Coelacanth-class_Pressure_Hull
+python gamerescape-recipe-sum.py -u https://ffxiv.gamerescape.com/wiki/Mythrite_Ingot -o output.csv
+python gamerescape-recipe-sum.py -u https://ffxiv.gamerescape.com/wiki/Medium_Cafe_Walls/Recipe
+```
+
+Use of a virtual environment is highly recommended:
 
 ```
 cd ffxiv-gamerescape-fc-recipe-sum
--- gamerescape-recipe-sum.py
+++ gamerescape-recipe-sum.py
@@ -1,10 +1,16 @@
+import argparse
 from collections import defaultdict
 import csv
-from sys import argv
 import urllib.request
 from urllib.parse import urlparse
 
 from bs4 import BeautifulSoup
+
+FC_CRAFT_TEXT = "Free Company Craft"    # Text that appears on a Gamer Escape page indicating an FC craft
+
+parser = argparse.ArgumentParser(description="FFXIV Gamer Escape Recipe Summer")
+parser.add_argument("-u", "--url", required=True, help="URL of a Gamer Escape wiki page for a craftable item")
+parser.add_argument("-o", "--output-file", help="Name of a CSV file to write sums to")
 
 def sanitize_url(s):
     parts = urlparse(s)
@@ -12,16 +18,25 @@ def sanitize_url(s):
     return s
 
 def sanitize_csv_file_name(s):
-    assert len(s) > 4
-    if s[-4:] != ".csv":
-        raise Exception("Filename must end in '.csv'")
+    if (len(s) >= 4 and s[-4:] != ".csv") or len(s) < 4:
+        s += ".csv"
     return s
+
+def get_cmd_line_input(inp: argparse.Namespace) -> tuple[str, str]:
+    url = sanitize_url(inp.url)
+
+    csv_filename = ""
+    if inp.output_file:
+        csv_filename = sanitize_csv_file_name(inp.output_file)
+    return (url, csv_filename)
 
 ################################
 
-url = sanitize_url(argv[1])
+url,csv_file = get_cmd_line_input(parser.parse_args())
 
-print(f"Finding recipes from: {url}")
+print(f"Finding recipes from:  {url}")
+if csv_file:
+    print(f"Will write to:         {csv_file}")
 
 req = urllib.request.Request(url)
 # workaround for the bot ban - apply to get this script approved!
@@ -29,22 +44,24 @@ req.add_header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0)
 with urllib.request.urlopen(req) as html_content:
     soup = BeautifulSoup(html_content, "html.parser")
 
+# If Gamer Escape makes any changes to their page layout, need to update (especially "IDENTIFIER"s)
 try:
-    recipes = soup.find_all(name="div", class_="itembox")
+    recipes_source = soup.find_all(name="div", class_="itembox")   # IDENTIFIER
 except:
     print("Could not find any recipes.")
     exit(1)
 
-# If Gamer Escape makes any changes to their page layout, "IDENTIFIER" lines must be changed!
-for i in recipes:
+print()
+
+all_recipes: list[tuple[str, defaultdict]] = [] # [(job, recipe), ... ]
+for i in recipes_source:
     recipe_soup = BeautifulSoup(str(i), "html.parser")
     first_a = recipe_soup.find(name="a")
-    is_free_company_craft = first_a.contents[0] == "Free Company Craft"
+    is_free_company_craft = first_a.contents[0] == FC_CRAFT_TEXT
     recipe = []
     recipe_title = ""
     if not is_free_company_craft:
         job = first_a.findNext(name="a").contents[0]
-        print()
         print("Detected standard recipe for", job)
         ingredients = recipe_soup.find_all(attrs={"align":"center", "width":"10%"})     # IDENTIFIER
         for j in range(0, len(ingredients), 2):
@@ -63,22 +80,34 @@ for i in recipes:
             ingredients.append(ingredient_soup.find(name="a").attrs['title'])
         quantities_source = recipe_soup.find_all(attrs={"style":"width: 1%; text-align: center;"})    # IDENTIFIER
         for k in range(1, len(quantities_source), 2):
+            # FC crafts are organized into "batches": 10/30 means 30 total items in batches of 10
+            # Iterating here goes through each item: batch size, then total size. We only want the totals, so these numbers:
+            #   10, 30, 6, 18, 3, 9
+            #       ^^     ^^     ^
             quantities.append(quantities_source[k].contents[0])
         assert len(ingredients) == len(quantities)
         for l,m in zip(ingredients, quantities):
             recipe.append((l,m))
-        recipe_title = "Free Company Craft"
+        recipe_title = FC_CRAFT_TEXT
     #print(f"    {recipe}")
     sums = defaultdict(int)
     for item,quantity in recipe:
         sums[item] += int(quantity)
 
+    all_recipes.append((recipe_title, sums))
+
+print()
+
+if all_recipes:
     print("Final recipe sums:")
-    print(f"  {recipe_title}")
-    if is_free_company_craft:
-        # For FC crafts: display results sorted by total quantity
-        for ing in sorted(sums.keys(), key=lambda v: int(sums[v])):
-            print(f"    {sums[ing]}x {ing}")
-    else:
-        for ing in sums:
-            print(f"    {sums[ing]}x {ing}")
+    for title,recipe in all_recipes:
+        print(f"  {title}")
+        if title == FC_CRAFT_TEXT:
+            # For FC crafts: display results sorted by total quantity
+            for ing in sorted(recipe.keys(), key=lambda v: recipe[v]):
+                print(f"    {recipe[ing]}x {ing}")
+        else:
+            for ing in recipe:
+                print(f"    {recipe[ing]}x {ing}")
+
+# TODO: write CSV file of everything in all_recipes
